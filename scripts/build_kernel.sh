812ec883eacc227ec88fd2320e7c3c3f8e3299a2
Ilya Matthew Kuvarzin <luceo2011@yandex.ru>
2023-11-10 20:57:16 +0100

    fix: build_kernel script fix

--- scripts/build_kernel.sh
+++ scripts/build_kernel.sh
@@ -1,1 +1,3 @@
-cd /home/android/lineage/out/kernel/samsung/gtexslte/ && ./build_kernel.sh && cd /home/android/lineage/
+ln -sf /home/android/lineage ~/android-work && \
+cd /home/android/lineage/kernel/samsung/gtexslte/ && \
+./build_kernel.sh && cd /home/android/lineage/
